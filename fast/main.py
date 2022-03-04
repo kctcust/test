3d661e550c789f9c0c34ca056f8eb15cef408450
kctcust <iei07@ems.tcust.edu.tw>
2022-03-04 20:04:25 +0800

    a

--- fast/main.py
+++ fast/main.py
@@ -6,7 +6,7 @@ app = FastAPI()  # 建立一個 Fast API application
 
 @app.get("/")  # 指定 api 路徑 (get方法)
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Worldaaa"}
 
 
 @app.get("/users/{user_id}")  # 指定 api 路徑 (get方法)
