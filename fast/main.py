f946a548da2c7c6f0cc4d2d8426a00631270eee6
kctcust <iei07@ems.tcust.edu.tw>
2022-03-05 12:14:05 +0800

    c

--- fast/main.py
+++ fast/main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
 
@@ -12,3 +13,35 @@ def read_root():
 @app.get("/users/{user_id}")  # 指定 api 路徑 (get方法)
 def read_user(user_id: int, q: Optional[str] = None):
     return {"user_id": user_id, "q": q}
+
+
+@app.get("/query_param_str")
+async def query_param_str(pID: str):  # 轉換成字串
+    return {"user": pID}
+
+
+@app.get("/query_param_int")
+async def query_param_int(pID: int):  # 轉換成整數
+    return {"user": pID}
+
+
+@app.get("/default_param")
+async def query_param_str(param_a: str, param_b: str = "example"):  # 轉換成字串
+    return {"param_a": param_a, "param_b": param_b}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
